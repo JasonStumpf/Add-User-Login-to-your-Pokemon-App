bddc6780937e9e332cc362418c0ee08120817ab7
Jason Stumpf <jstumpf518@yahoo.com>
2023-06-16 23:35:52 -0700

    First Commit :)

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
